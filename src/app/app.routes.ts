ced034b57f1f5774934f9d15dd187d7fea060790
Hossein heydarpour <hosseinheydarpour1@gmail.com>
2026-06-15 11:00:28 +0330

    feat: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { RecipeDetailComponent } from './features/recipes/pages/recipe-detail/recipe-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: RecipeDetailComponent,
+	},
+	{
+		path: '**',
+		component: RecipeDetailComponent,
+	},
+];
